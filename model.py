bf2f600de2007a8eab562a70851e9aaa7c4896dd
rishabh verma <29885265+rv0001@users.noreply.github.com>
2019-07-03 16:29:46 +0530

    added all the pythoncode

---
+++ model.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+import requests
+import json
+
+# Importing the dataset
+dataset = pd.read_csv('/home/rishabh/projects/flask app/flask-salary-predictor/Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Saving model to disk
+pickle.dump(regressor, open('/home/rishabh/projects/flask app/flask-salary-predictor/model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load( open('/home/rishabh/projects/flask app/flask-salary-predictor/model.pkl','rb'))
+print(model.predict([[1.8]]))
